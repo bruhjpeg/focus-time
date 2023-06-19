c9b689d4590579750a09ea60b4e3bdcbf99048eb
BrunaAquino22 <brunahasting99@gmail.com>
2023-06-19 18:10:00 -0300

    .

---
+++ main.js
@@ -0,0 +1,72 @@
+const buttonPlay = document.querySelector(".play");
+const buttonPause = document.querySelector(".pause");
+const buttonSet = document.querySelector(".set");
+const buttonStop = document.querySelector(".stop");
+const buttonSoundOn = document.querySelector(".sound-on");
+const buttonSoundOff = document.querySelector(".sound-off");
+let minutes;
+const minutesDisplay = document.querySelector(".minutes");
+const secondsDisplay = document.querySelector(".seconds");
+
+buttonPlay.addEventListener("click", cliqueiButtonPlay);
+buttonPause.addEventListener("click", cliqueiButtonPause);
+buttonStop.addEventListener("click", cliqueiButtonStop);
+buttonSoundOn.addEventListener("click", cliqueiButtonSoundOn);
+buttonSoundOff.addEventListener("click", cliqueiButtonSoundOff);
+buttonSet.addEventListener("click", horarioSet);
+
+function countdown() {
+  setTimeout(function () {
+    let seconds = Number(secondsDisplay.textContent);
+    let minutes = Number(minutesDisplay.textContent);
+
+    if (seconds <= 0) {
+      seconds = 60;
+      minutesDisplay.textContent = minutes - 1;
+    }
+
+    if(minutes <= 0) {
+        
+    }
+
+    secondsDisplay.textContent = String(seconds - 1).padStart(2, "0");
+
+    countdown();
+  }, 1000);
+}
+
+function cliqueiButtonPlay() {
+  buttonPlay.classList.toggle("hide");
+  buttonPause.classList.toggle("hide");
+  buttonSet.classList.add("hide");
+  buttonStop.classList.remove("hide");
+
+  countdown();
+}
+
+function cliqueiButtonPause() {
+  buttonPause.classList.toggle("hide");
+  buttonPlay.classList.toggle("hide");
+}
+
+function cliqueiButtonStop() {
+  buttonPlay.classList.remove("hide");
+  buttonPause.classList.add("hide");
+  buttonStop.classList.add("hide");
+  buttonSet.classList.remove("hide");
+}
+
+function cliqueiButtonSoundOn() {
+  buttonSoundOn.classList.add("hide");
+  buttonSoundOff.classList.remove("hide");
+}
+
+function cliqueiButtonSoundOff() {
+  buttonSoundOn.classList.remove("hide");
+  buttonSoundOff.classList.add("hide");
+}
+
+function horarioSet() {
+  minutes = prompt("Quantos minutos?");
+  minutesDisplay.textContent = minutes;
+}
